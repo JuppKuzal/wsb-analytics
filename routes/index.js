b9e25655130c38e1125ae52bbb38e744234d837b
JuppKuzal <pepa.kousal@gmail.com>
2021-01-16 18:27:42 +0100

    get all, by tckr, post,delete by tckr -not tested!

--- routes/index.js
+++ routes/index.js
@@ -14,14 +14,49 @@ client.connect(err => {
   client.close();
 });
 
+/********************* */
 
-/* GET stock data */
+/* Util */
+
+function isValidStockEntry(stockEntry) {
+  return !(stockEntry.ticker && stockEntry.amount);
+}
+
+
+/* GET all stock data */
 router.get('/', (req, res) => {
   var stockData = [];
 
   try{
+    const dbdata = await collection.get();
+    
+    if(dbdata.empty) {
+      // 404 not found
+      res.status(404).send();
+    }
+    
+    //fill all
+    dbdata.forEach((doc) => {
+      var entry = doc.data();
+      entry.id = doc.id;
+      stockData.push(entry);
+    });
+
+    res.status(200).send(stockData);
+  } catch (error) {
+    //500 internal server error
+    console.error("Error: " + JSON.stringify(error));
+    res.status(500).send();
+  }
+});
+
+/* GET stock data by ticker*/
+router.get('/:ticker', (req, res) => {
+  var stockData = [];
+
+  try{
     //catch bad req
-    if(req.query.stockTicker === undefined) {
+    if(req.params.stockTicker === undefined) {
       res.status(400).send();
     }
     const dbdata = await collection
@@ -42,17 +77,68 @@ router.get('/', (req, res) => {
     res.status(200).send(stockData);
   } catch (error) {
     //500 internal server error
-    console.log("Error: " + JSON.stringify(error));
+    console.error("Error: " + JSON.stringify(error));
     res.status(500).send();
   }
 });
 
 router.post('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //init
+  var stockEntry = req.body;
+
+  //400 bad req
+  if(isValidStockEntry(stockEntry)) {
+    res.status(400).send();
+    return;
+  }
+  try {
+    //fill request with request body
+    const docRef = collection.doc();
+    const createTime = Date.now();
+    await docRef.set({
+      ticker: stockEntry.ticker,
+      amount: stockEntry.amount,
+      price: stockEntry.price
+    })
+    stockEntry.time = createTimel
+    comment.id = docRef.id;
+    //send post res
+    res.status(201).send(stockEntry);
+  } catch (error) {
+    //catch 500 internal server error
+    console.error("Error: " + JSON.stringify(error));
+    res.status(500).send();
+  }
 });
 
-router.delete('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//delete by stock ticker in params
+router.delete('/:ticker', function(req, res, next) {
+  
+  try{
+
+    if(req.query.ticker === undefined){
+      res.status(400).send();
+    }
+
+    const dbdata = await collection.where('ticker', '==', req.params.ticker).get();
+
+    if (!dbdata.empty) {
+      dbdata.forEach((doc) => {
+        doc.ref.delete();
+      });
+
+      res.status(204).send();
+    } else {
+      //catch 404 not found
+      res.status(404).send();
+      return;
+    }
+  } catch (error) {
+    //catch 500 internal server error
+    console.error("Error: " + JSON.stringify(error));
+    res.status(500).send();
+  }
+
 });
 
 module.exports = router;
